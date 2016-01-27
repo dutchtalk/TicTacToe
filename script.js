fb03ae3a2aa1c7ad6eb09e3a1ff78a74a23e78c8
mikaturk <mikaturk@me.com>
2016-01-27 16:37:48 +0100

    Checked script.js for errors
    http://jslint.com was used to check the js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-console.log('script.js called')
+'use strict';
+if (!typeof $ == "function") {function $() {alert('jQuery is not loaded!');}
 $(document).ready(function(){
-console.log('document loaded')
+console.log('document loaded');
 //Javascript for index.html
 //If 'Turn' equal true, an X will be placed. Otherwise an O will be placed
 var Turn = true;
@@ -13,42 +14,42 @@ var ButtonValue;
 var InProgress = true;
 
 //Function that gets triggered on every button press. value being what button is pressed
-console.log('Creating ChangeValue()')
+console.log('Creating ChangeValue()');
 function ChangeValue(value) {
-	console.log('ChangeValue() created')
-	//Makes sure you can't play/change buttons when the game is over
-	if(InProgress)
-	{
-	ButtonToChange = value + "Button";
+  console.log('ChangeValue() created');
+  //Makes sure you can't play/change buttons when the game is over
+  if (InProgress)
+  {
+  ButtonToChange = value + "Button";
 
-	//Fill ButtonValue with the value of the button so you can check it in the following if statement
-	ButtonValue = document.getElementById(ButtonToChange).innerText;
-	//Check if ButtonValue is "-" (thus not a filled button yet)
-	if(ButtonValue == "-")
-	{
-		//If Turn equals true it's the X's turn
-		if(Turn)
-		{
-			//Change the button value to X
-			document.getElementById(ButtonToChange).innerText = "X";
-		}
-		//If Turn equals false it's the O's turn
-		else
-		{
-			//Change the button value to O
-			document.getElementById(ButtonToChange).innerText = "O";
-		}
-	}
-	else
-	{
-		//Do nothing
-		Turn = !Turn;
-	}
-	CheckWinners();
-	}
-	Turn = !Turn;
-};
-console.log('done creating ChangeValue()')
+  //Fill ButtonValue with the value of the button so you can check it in the following if statement
+  ButtonValue = document.getElementById(ButtonToChange).innerText;
+  //Check if ButtonValue is "-" (thus not a filled button yet)
+  if (ButtonValue == "-")
+  {
+    //If Turn equals true it's the X's turn
+    if (Turn)
+    {
+      //Change the button value to X
+      document.getElementById(ButtonToChange).innerText = "X";
+    }
+    //If Turn equals false it's the O's turn
+    else
+    {
+      //Change the button value to O
+      document.getElementById(ButtonToChange).innerText = "O";
+    }
+  }
+  else
+  {
+    //Do nothing
+    Turn = !Turn;
+  }
+  new CheckWinners();
+  }
+  Turn = !Turn;
+}
+console.log('done creating ChangeValue()');
 winningScenarios = [
   ["1","2","3"], //left to right
   ["4","5","6"],
@@ -65,38 +66,38 @@ function innertext(id) {
 }
 
 function CheckWinners() {
-  for (a = 0; a < winningscenarios.length; a++) {
+  for (a = 0; a < winningscenarios.length; a+=1) {
       if (innertext(ButtonsArray[winningScenarios[a][0]]) ==
       innertext(ButtonsArray[winningScenarios[a][1]]) &&
       innertext(ButtonsArray[winningScenarios[a][0]]) ==
       innertext(ButtonsArray[winningScenarios[a][2]])) {
-        Win(innertext(ButtonsArray[winningScenarios[a][0]]))
+        new Win(innertext(ButtonsArray[winningScenarios[a][0]]));
       }
   }
-};
+}
 
 
 //If there is someone with three the same symbols in a row (decided in CheckWinners())
 //this function will tell who is the winner
 function Win(Winning)
 {
-	//Makes sure you can't change buttons if the game is ended
-	InProgress = false;
-	//Tells who is the winner (Winning being the value that gets passed to the function 'Win').
-	document.getElementById("WinnerText").innerText = Winning + " Has won!";
-};
+  //Makes sure you can't change buttons if the game is ended
+  InProgress = false;
+  //Tells who is the winner (Winning being the value that gets passed to the function 'Win').
+  document.getElementById("WinnerText").innerText = Winning + " Has won!";
+}
 
 //Function for the button to reset the game.
 //Turns all button to their default ("-") and also makes it so you can play again
 function Reset()
 {
-	//Loop that makes all the buttons reset
-	for (i = 0; i < ButtonsArray.length; i++)
-	{
-		document.getElementById(ButtonsArray[i]).innerText = "-";
-	}
-	//Change the text that shows who won to nothing, so it gets removed.
-	document.getElementById("WinnerText").innerText = "";
-	InProgress = true;
+  //Loop that makes all the buttons reset
+  for (i = 0; i < ButtonsArray.length; i+=1)
+  {
+    document.getElementById(ButtonsArray[i]).innerText = "-";
+  }
+  //Change the text that shows who won to nothing, so it gets removed.
+  document.getElementById("WinnerText").innerText = "";
+  InProgress = true;
 }
 });
